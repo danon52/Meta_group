bc6c2e726693956ce86cd4f2c9d6b528750b4818
Danil Kolbasenko <danilvusockij@gmail.com>
2026-06-17 21:31:12 +0300

    Install react-router

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import { createBrowserRouter } from 'react-router'
+
+// export default function routes() {
+//     const router = createBrowserRouter([
+//         {
+
+//         }
+//     ])
+
+
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
